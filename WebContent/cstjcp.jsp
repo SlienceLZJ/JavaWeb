8576aa5ea6204e77ae0953a444bb82f7c77ea528
Yuechao  Li <Yuechao  Li@DESKTOP-E3198Q5>
2019-11-07 20:25:03 +0800

    1

---
+++ WebContent/cstjcp.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>测试添加</title>
+</head>
+<body>
+<table><tr><td>sadds</td></tr><tr>asfsad<tr></table>
+<p>请输入您想添加的菜品</p>
+</body>
+</html>
---
+++ src/com/domain/Dian.java
@@ -0,0 +1,62 @@
+package com.domain;
+
+
+public class Dian {
+	private int id;
+	private String name;
+	private String picture;
+	private String foodname;
+	private String foodpicture;
+	private String foodprice;
+	private String foodIntroduction;
+	private String foodtype;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public String getFoodname() {
+		return foodname;
+	}
+	public void setFoodname(String foodname) {
+		this.foodname = foodname;
+	}
+	public String getFoodpicture() {
+		return foodpicture;
+	}
+	public void setFoodpicture(String foodpicture) {
+		this.foodpicture = foodpicture;
+	}
+	public String getFoodprice() {
+		return foodprice;
+	}
+	public void setFoodprice(String foodprice) {
+		this.foodprice = foodprice;
+	}
+	public String getFoodIntroduction() {
+		return foodIntroduction;
+	}
+	public void setFoodIntroduction(String foodIntroduction) {
+		this.foodIntroduction = foodIntroduction;
+	}
+	public String getFoodtype() {
+		return foodtype;
+	}
+	public void setFoodtype(String foodtype) {
+		this.foodtype = foodtype;
+	}
+}
+
