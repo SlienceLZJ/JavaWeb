df62bad1cf1c437963bd0a3c221a014a13f6a5aa
贺振原 <2290225756@qq.com>
2019-11-08 08:48:14 +0800

    1

--- WebContent/dianming.jsp
+++ WebContent/dianming.jsp
@@ -1,18 +1,56 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ page import="java.util.*,com.domain.DianMingInformation" %>
+       
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+ 
+ 
+  
+    
 <!DOCTYPE html>
 <html>
 <head>
+
+ <style>
+.box{width:150px; text-align:center; font-szie:18px;}
+.box img {width:100%;}
+</style> 
+
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>点餐系统</title>
 </head>
+
 <body>
 
 
+<%
+List<DianMingInformation> list=(List<DianMingInformation>)request.getAttribute("list");
+%>
+
+<table>
 
 
-hello world;
+ <tr>
 
+<%
+for(int i=0;i<list.size();i++){
+   DianMingInformation info=list.get(i);
+   
+   if(i%3==0){
+	   out.println("</tr><tr>");
+   }
+   else{
+	   out.println("<td><div class='box'><img src='"+info.getPicture() +"'/>"+info.getName()+"</div></td>");
+	   System.out.println(info.getName());
+	   
+   }
+   
+}
+
+%>
+
+</tr>
+</table>
 
 
 
--- src/com/servlet/DianMing.java
+++ src/com/servlet/DianMing.java
@@ -27,41 +27,44 @@ public class DianMing extends HttpServlet {
           
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doPost(request, response);
+		
+		doPost(request, response);		
 	}
-
-	
+		
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		String sql="select * from DianMing";
 		ResultSet rs=JDBCDao.getData(sql);
-		DianMingInformation info=new DianMingInformation();	
-		
+			
 		List<DianMingInformation> list=new ArrayList<DianMingInformation>();
-		
+						
 		try {
 								
 			while(rs.next()) {
+			DianMingInformation info=new DianMingInformation();	
 			info.setName(rs.getString("name"));
 			info.setPicture(rs.getString("picture"));	
-			
+			list.add(info);
+			System.out.println("resut set is "+rs.getString("name"));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		request.setAttribute("list", list);
+	
+		
+		System.out.println("the size is :"+list.size());
+		System.out.println("the first is "+list.get(0).getName());
+		System.out.println("the second is "+list.get(1).getName());
 		
 		 RequestDispatcher rd = request.getRequestDispatcher("dianming.jsp");
 		 rd.forward(request, response);
-		
-		
-		
-		
-		
-		
-		
+	
+		 
+		 
+		 
 	}
 
 }
